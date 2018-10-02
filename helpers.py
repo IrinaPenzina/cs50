405690f17705f56b548cba04b9c4a586d6d29e70
IrinaPenzina <IrinaPenzina@users.noreply.github.com>
2018-10-02 03:41:06 +0000

    20181002T034051Z

---
+++ helpers.py
@@ -0,0 +1,25 @@
+from cs50 import get_int
+from nltk.tokenize import sent_tokenize
+
+def lines(a, b):
+    """Return lines in both a and b"""
+    # return the list of similar lines
+    return list(set(a.split('\n')).intersection(b.split('\n')))
+
+def sentences(a, b):
+    """Return sentences in both a and b"""
+    # return the list of similar sentences
+    return list(set(sent_tokenize(a)).intersection(sent_tokenize(b)))
+
+def substrings(a, b, n):
+    """Return substrings of length n in both a and b"""
+    # return the list of similar sentences
+    #for i in range (len(a)+n):
+     #   asub = a[i:i+n]
+      #  i+=n
+    #for i in range(len(b)+n):
+     #   bsub = b[i:i+n]
+      #  i+=n
+    #return list(set(asub).intersection(bsub))
+    return list(set([a[i:i+n] for i in range(len(a) -(n-1))]).intersection([b[i:i+n] for i in range(len(b) -(n-1))]))
+
